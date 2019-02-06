bdfee124426ea3cb79a7d98f06cb9cf447f351c3
WesAnt <47301142+WesAnt@users.noreply.github.com>
2019-02-06 23:09:44 +0000

    Add files via upload

---
+++ GameOfLife.java
@@ -0,0 +1,12 @@
+
+public class GameOfLife {
+	
+	public static void main(String[] args) {
+		Gui gui = new Gui(); // Create the main Graphical User Interface object
+		
+		while(true) {
+			gui.repaint();	
+		}
+	}	
+			
+}
